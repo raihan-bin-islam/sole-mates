1b2173e15557f2509c8c5117cf47510726895775
raihan-bin-islam <rishaon0@gmail.com>
2022-01-13 19:03:13 +0600

    add category section: added a list of categories to the home page for easier navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import FeaturedProducts from "./components/FeaturedProducts";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <div>
