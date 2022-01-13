97c7361b3c0a1276a0b34f6793482f8425e71b25
raihan-bin-islam <rishaon0@gmail.com>
2022-01-13 19:13:32 +0600

    create category: created category component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,23 +1,16 @@
 import React from "react";
-import { useEffect } from "react/cjs/react.development";
 import categoryList from "../utils/categoryList";
 const Categories = () => {
-  useEffect(() => {
-    console.log(categoryList);
-  });
   return (
-    <div className="categories">
+    <div className="category">
       <h1>Shop Now</h1>
-      <div className="category-list">
-        {categoryList.map(({ id, category, thumbnail }) => {
-          return (
-            <div className="category-card" key={id}>
-              <h2>{category.toUpperCase()}</h2>
-              <img src={thumbnail} alt="" />
-            </div>
-          );
-        })}
-      </div>
+      <div className="category-list"></div>
+      {categoryList.map(({ id, category, thumbnail }) => {
+        <div className="category-card" key={id}>
+          <h2>{category}</h2>
+          <img src={thumbnail} alt="" />
+        </div>;
+      })}
     </div>
   );
 };
