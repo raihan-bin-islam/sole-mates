d672b3eed7f260e6e648260568e759218c895baf
raihan-bin-islam <rishaon0@gmail.com>
2022-01-14 00:37:36 +0600

    add all routes to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Home from "./routes/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Men from "./routes/Men";
+import Women from "./routes/Women";
+import Kids from "./routes/Kids";
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/men" element={<Men />}></Route>
+          <Route path="/women" element={<Women />}></Route>
+          <Route path="/kids" element={<Kids />}></Route>
         </Routes>
       </Router>
     </>
