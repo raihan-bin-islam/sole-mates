39c5bb4e2067c1cb4e7eb93895d23f7d3c4c8877
raihan-bin-islam <rishaon0@gmail.com>
2022-01-15 04:42:55 +0600

    change fonts and add copyright section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,35 +4,40 @@ import NavigationLinks from "./NavigationLinks";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="footer-container">
-        <div className="website-details">
-          <h3 className="logo">Sole Mates</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quod, nisi suscipit iusto
-            pariatur quis iure necessitatibus tempore nostrum soluta!
-          </p>
-          <div className="social-icons">
-            <a href="https://www.facebook.com/raihanulislams/">
-              <BsFacebook className="icons" />
-            </a>
-            <a href="https://www.instagram.com/raihanul_islam">
-              <BsInstagram className="icons" />
-            </a>
-            <a href="https://www.linkedin.com/in/raihan-bin-islam/">
-              <BsLinkedin className="icons" />
-            </a>
-            <a href="https://github.com/raihan-bin-islam/">
-              <BsGithub className="icons" />
-            </a>
+    <>
+      <div className="footer">
+        <div className="footer-container">
+          <div className="website-details">
+            <h3 className="logo">Sole Mates</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quod, nisi suscipit
+              iusto pariatur quis iure necessitatibus tempore nostrum soluta!
+            </p>
+            <div className="social-icons">
+              <a href="https://www.facebook.com/raihanulislams/">
+                <BsFacebook className="icons" />
+              </a>
+              <a href="https://www.instagram.com/raihanul_islam">
+                <BsInstagram className="icons" />
+              </a>
+              <a href="https://www.linkedin.com/in/raihan-bin-islam/">
+                <BsLinkedin className="icons" />
+              </a>
+              <a href="https://github.com/raihan-bin-islam/">
+                <BsGithub className="icons" />
+              </a>
+            </div>
+          </div>
+          <div className="footer-links">
+            <h3>Useful Links</h3>
+            <NavigationLinks />
           </div>
         </div>
-        <div className="footer-links">
-          <h3>Useful Links</h3>
-          <NavigationLinks />
-        </div>
       </div>
-    </div>
+      <div className="copyright-section">
+        <p>&copy;2022 Raihan Bin Islam</p>
+      </div>
+    </>
   );
 };
 
