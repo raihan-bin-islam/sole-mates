403e76f4fa56a1a606876340559b0d5badb16b89
raihan-bin-islam <rishaon0@gmail.com>
2022-01-18 01:09:05 +0600

    add smaller image: took lower sized image from the API for faster loading time

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const ProductCard = ({ data }) => {
   const {
-    media: { imageUrl },
+    media: { thumbUrl },
     title,
     gender,
     retailPrice,
   } = data; // grab basic details from the product data
   return (
     <>
-      {imageUrl && (
+      {thumbUrl && (
         <div className="product-card">
-          <img src={imageUrl} alt="" />
+          <img src={thumbUrl} alt="" />
           <p className="name">{title}</p>
           <p className="gender">{gender.toUpperCase()}</p>
           <p className="price">${retailPrice}</p>
