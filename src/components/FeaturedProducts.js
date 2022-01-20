fd801f0433dbcdda3150709af6ba34451aaf2a4e
raihan-bin-islam <rishaon0@gmail.com>
2022-01-21 03:14:07 +0600

    minor changes

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -4,13 +4,10 @@ import Products from "./Products";
 const FeaturedProducts = () => {
   const heading = "Featured Products";
   const [page, setPage] = useState(1);
-  const nextPage = () => {
-    setPage((prevPage) => prevPage + 1);
-  };
+
   return (
     <>
       <Products heading={heading} year={2020} page={page} numberOfProducts={3} />
-      <button onClick={nextPage}>next</button>
     </>
   );
 };
